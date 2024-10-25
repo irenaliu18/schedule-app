fbf90e4087f78c1604be95230cdd9567ebe9c962
irena liu <irenaliu18@gmail.com>
2024-10-25 16:50:19 -0500

    Fixed styling

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { signInWithGoogle, firebaseSignOut, useAuth } from '../../utilities/firebase';
 
-const Banner = ({ title }) => {
+const Banner = () => {
   const user = useAuth();
 
   return (
     <div className="banner">
-      <h1>{title}</h1>
-      {user ? (
-        <button onClick={firebaseSignOut}>Sign Out ({user.displayName})</button>
-      ) : (
-        <button onClick={signInWithGoogle}>Sign In with Google</button>
-      )}
+      <div className="banner-content">
+        <h1>CS Courses for 2018-2019</h1>
+        {user ? (
+          <button onClick={firebaseSignOut}>Sign Out ({user.displayName})</button>
+        ) : (
+          <button onClick={signInWithGoogle}>Sign In with Google</button>
+        )}
+      </div>
     </div>
   );
 };
